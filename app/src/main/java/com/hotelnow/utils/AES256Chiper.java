aa254a34da0f6b4e4efc6ca59de3e386f683a014
황인대 <idhwang@hotelnow.co.kr>
2019-03-12 16:15:01 +0900

    개인정보 암호화 부분 수정

--- app/src/main/java/com/hotelnow/utils/AES256Chiper.java
+++ app/src/main/java/com/hotelnow/utils/AES256Chiper.java
@@ -12,37 +12,47 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class AES256Chiper {
     public static byte[] ivBytes = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-    public static String secretKey = "12345678901234567890120612461242";
+    public static String secretKey = "ABCDEF0123456124";
 
     //AES256 암호화
-    public static String AES_Encode(String str)    throws java.io.UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+    public static String AES_Encode(String str) {
 
-        byte[] textBytes = str.getBytes("UTF-8");
-        AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
-        SecretKeySpec newKey = new SecretKeySpec(secretKey.getBytes("UTF-8"), "AES");
-        Cipher cipher = null;
-        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec);
-
-        return Base64.encodeToString(cipher.doFinal(textBytes), 0);
+        byte[] encryptedText = null;
+        try {
+            byte[] keyData = secretKey.getBytes();
+            SecretKey ks = new SecretKeySpec(keyData, "AES");
+            Cipher c = Cipher.getInstance("AES");
+            c.init(Cipher.ENCRYPT_MODE, ks);
+            encryptedText = c.doFinal(str.getBytes("UTF-8"));
+            return Base64.encodeToString(encryptedText, Base64.DEFAULT);
+        } catch (Exception e) {
+            LogUtil.e("xxxx", e.toString());
+            return "";
+        }
     }
 
     //AES256 복호화
-    public static String AES_Decode(String str)    throws java.io.UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+    public static String AES_Decode(String str) {
 
         if(!TextUtils.isEmpty(str)) {
-            byte[] textBytes = Base64.decode(str, 0);
-            //byte[] textBytes = str.getBytes("UTF-8");
-            AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
-            SecretKeySpec newKey = new SecretKeySpec(secretKey.getBytes("UTF-8"), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, newKey, ivSpec);
-            return new String(cipher.doFinal(textBytes), "UTF-8");
+            byte[] clearText = null;
+            try {
+                byte[] keyData = secretKey.getBytes();
+                SecretKey ks = new SecretKeySpec(keyData, "AES");
+                Cipher c = Cipher.getInstance("AES");
+                c.init(Cipher.DECRYPT_MODE, ks);
+                clearText = c.doFinal(Base64.decode(str, Base64.DEFAULT));
+                return new String(clearText, "UTF-8");
+            } catch (Exception e) {
+                LogUtil.e("xxxx", e.toString()+"");
+                return "";
+            }
         }
         else{
             return "";
