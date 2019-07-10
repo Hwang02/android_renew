7620829a4389729d115f0f59cf991457a8014951
황인대 <idhwang@yanolja.com>
2019-07-10 10:13:40 +0900

    상세화면 Locale.KOREAN 추가

--- app/src/main/java/com/hotelnow/activity/DetailHotelActivity.java
+++ app/src/main/java/com/hotelnow/activity/DetailHotelActivity.java
@@ -93,6 +93,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
 
@@ -128,7 +129,7 @@ public class DetailHotelActivity extends AppCompatActivity implements OnMapReady
     private SharedPreferences _preferences;
     private String cookie;
     private String[] selectList;
-    private SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+    private SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREAN);
     private String lodge_type;
     private int privatedeal_status = -1;
     private DialogAlert dialogAlert = null;
